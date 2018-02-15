1155aefe84c5a22ab986a1888cbb928f972300f5
sandyRS <36299245+sandyRS@users.noreply.github.com>
2018-02-15 14:44:51 +0530

    Create calculater.py

---
+++ calculater.py
@@ -0,0 +1,11 @@
+n=int(input('enter your input'))
+a=int(input("enter A value"))
+b=int(input("enter B value"))
+if(n==1):
+    print(a+b)
+if(n==2):
+    print(a-b)
+if(n==3):
+    print(a*b)
+if(n==4):
+    print(a/b)
