f24057438b69395f3d8b85b076c7d14870ae50c4
sandyRS <36299245+sandyRS@users.noreply.github.com>
2018-03-15 17:20:42 +0530

    Update primeornot.py

--- primeornot.py
+++ primeornot.py
@@ -2,7 +2,7 @@ a=int(input("enter your input"))
 if a>1:
   for i in range (2,a):
     if(a%i==0):
-      print("not a prime number")
+      print("no")
       break
   else:
-    print("prime number")
+    print("yes")
