c19dfc45c990fdb8b366a21335a1d1d60741601c
sandyRS <36299245+sandyRS@users.noreply.github.com>
2018-04-04 11:33:09 +0530

    Update santhiya38.py

--- santhiya38.py
+++ santhiya38.py
@@ -1,6 +1,6 @@
-val=int(input())
-lis=[]
-for i in range(2,val+1,2):
-    if val%i==0:
-        lis.append(i)
-print(" ".join (str(i) for i in lis))
+v=int(input("enter"))
+l=[]
+for i in range(2,v+1,2):
+    if v%i==0:
+        l.append(i)
+print(" ".join (str(i) for i in l))
