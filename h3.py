15d7cd2183ea05cf29bb0ffe406845585a7f1cf5
sandyRS <36299245+sandyRS@users.noreply.github.com>
2018-05-24 11:18:07 +0530

    Update h3.py

--- h3.py
+++ h3.py
@@ -1,4 +1,5 @@
 l=[]
+s=[]
 a=int(input())
 for i in range(1,a+1):
   n=int(input())
@@ -6,4 +7,5 @@ for i in range(1,a+1):
 for i in l:
   a=l.index(i)  
   if(i==a):
-    print(i)
+    s.append(i)
+print("".join(str(i) for i in s))
